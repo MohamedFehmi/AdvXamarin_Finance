8fe2a3dd56b934e0409630e54395dca47750f983
Aloui MohamedFehmi <mohamedfehmi.aloui@esprit.tn>
2019-05-15 00:48:39 +0200

    The project working

---
+++ Finance.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "3177ed35-c368-43cd-a7e8-abcde3cfddd6",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
